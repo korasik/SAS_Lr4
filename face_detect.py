a4d937c56038beb7248adf374fc805955dc78215
korasik <50593694+korasik@users.noreply.github.com>
2020-02-16 23:54:03 +0300

    Update face_detect.py

--- face_detect.py
+++ face_detect.py
@@ -19,16 +19,8 @@ def on_off_ident(channel):
         on_off_id = 0
 
 on_off_id = 0
-on_off_led = 0
 
 prev_x = 320
-
-# GPIO.setmode(GPIO.BOARD)
-# GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-# GPIO.setup(LED_PIN, GPIO.OUT)
-# 
-# GPIO.add_event_detect(BUTTON_PIN, GPIO.RISING, bouncetime=300)
-# GPIO.add_event_callback(BUTTON_PIN, on_off_ident)
 
 try:
     GPIO.setmode(GPIO.BOARD)
@@ -38,11 +30,7 @@ try:
     GPIO.add_event_detect(BUTTON_PIN, GPIO.RISING, bouncetime=300)
     GPIO.add_event_callback(BUTTON_PIN, on_off_ident)
     
-    #faceCascade = cv2.CascadeClassifier('/home/pi/opencv-4.1.2/data/haarcascades/haarcascade_fullbody.xml')
-    #faceCascade = cv2.CascadeClassifier('/home/pi/opencv-4.1.2/data/haarcascades/haarcascade_profileface.xml')
     faceCascade = cv2.CascadeClassifier('/home/pi/opencv-4.1.2/data/haarcascades/haarcascade_frontalface_alt.xml')
-    #faceCascade = cv2.CascadeClassifier('/home/pi/opencv-4.1.2/data/haarcascades/haarcascade_frontalface_default.xml')
-    #faceCascade = cv2.CascadeClassifier('/home/pi/opencv-4.1.2/data/haarcascades/haarcascade_upperbody.xml')
     
     cap = cv2.VideoCapture(0)
     cap.set(3, 640)
@@ -50,7 +38,6 @@ try:
     
     while True:
         ret, img = cap.read()
-        #print('button:', GPIO.input(BUTTON_PIN))
         if on_off_id == 1:
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             faces = faceCascade.detectMultiScale(
@@ -60,13 +47,9 @@ try:
                 minSize=(20, 20)
             )
             if len(faces) > 0:
-                #print('LED ON')
                 GPIO.output(LED_PIN, True)
-                on_off_led = 1
             else:
-                #print('LED OFF')
                 GPIO.output(LED_PIN, False)
-                on_off_led = 0
 
             for (x,y,w,h) in faces:
                 cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
@@ -92,17 +75,12 @@ try:
 
     cap.release()
     cv2.destroyAllWindows()
-    if on_off_led == 1:
-        GPIO.output(LED_PIN, False)
-        on_off_led = 0
-    #GPIO.cleanup()
+        
 except KeyboardInterrupt:
     cap.release()
     cv2.destroyAllWindows()
-    #GPIO.output(LED_PIN, False)
     GPIO.cleanup()
 finally:
     cap.release()
     cv2.destroyAllWindows()
-    #GPIO.output(LED_PIN, False)
     GPIO.cleanup()
